cb10330416af90e2be345a9ff5f7e41e19d9b323
AbouYounes <karim.khattou@univ-tiaret.dz>
2024-04-18 16:59:56 +0200

    add customer view in urls/views/base/index

--- invoice_app/views.py
+++ invoice_app/views.py
@@ -19,3 +19,13 @@ class HomeView(View):
     def post(self, request, *args, **kwagrs):
         # modify an invoice
         return render(request, self.templates_name, self.context)
+    
+class AddCustomerView(View):
+     """ add new customer """    
+     template_name = 'add_customer.html'
+
+     def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+     def post(self, request, *args, **kwargs):
+        return render(request, self.template_name) 
